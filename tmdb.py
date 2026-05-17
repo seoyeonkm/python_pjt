1a40e4397fbb6b7c2ce27331b24645c2b670e8ae
tmdvltuf123 <tmdvltuf123@gmail.com>
2026-05-17 14:03:50 +0900

    kobis api added

--- tmdb.py
+++ tmdb.py
@@ -13,7 +13,7 @@ url = "https://api.themoviedb.org/3/movie/popular"
 
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZmVmYzNkZTk1OTEzOWQ4ZTdiYWIzMWQwMDc2MTA1YSIsIm5iZiI6MTc3ODkzMTI3My43MSwic3ViIjoiNmEwODU2NDllYTA2MWEwNzVmNjY0M2IzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.g6yVrbVf5V9gjapMm_KJ3X2BOAnBvcHYWPhhm4X5R1c"
+    "Authorization": "Bearer " + TMDB_API_KEY
 }
 
 response = requests.get(url, headers=headers)
