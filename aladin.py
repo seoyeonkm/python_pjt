040bb8f24fb076d2746ad976cb0d8883aa8a1216
tmdvltuf123 <tmdvltuf123@gmail.com>
2026-05-20 22:58:44 +0900

    add

--- aladin.py
+++ aladin.py
@@ -1,29 +1,58 @@
 import requests
+import pandas as pd
+import time
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 ALADIN_OPEN_API_KEY = os.getenv("ALADIN_OPEN_API_KEY")
 
-book_item_list_url = "https://www.aladin.co.kr/ttb/api/ItemList.aspx"
-book_item_list_params = {
-    "ttbkey": ALADIN_OPEN_API_KEY,
-    "QueryType": "BestSeller",
-    "SearchTarget": "Book",
-    #"subSearchTarget":
-    #"start":
-    #"MaxReasults":
-    #"Cover":
-    #"CategoryId":
-    "output": "js",
-    #"Partner":
-    #"includeKey":
-    #"InputEncoding":
-    "Version": "20131101",
-    #"outofStockFilter":
-    #"Year,Month,Week":
+genre_targets = {
+    '소설': '1',
+    '경제/경영': '170',
+    '인문': '656', 
+    '사회': '798',
+    '과학': '987',
+    'IT/컴퓨터': '351',
+    '자기계발': '336'
 }
 
-response = requests.get(url=book_item_list_url, params=book_item_list_params)
-print(response.text)
+def get_balanced_bestseller():
+    all_books = []
+    url = "https://www.aladin.co.kr/ttb/api/ItemList.aspx"
+    
+    for genre_name, cid in genre_targets.items():
+        params = {
+            "ttbkey": ALADIN_OPEN_API_KEY,
+            "QueryType": "BestSeller",
+            "SearchTarget": "Book",
+            "CategoryId": cid,
+            "MaxResults": 28,
+            "start": 1,
+            "output": "js",
+            "Version": "20131101"
+        }
+        
+        response = requests.get(url, params=params)
+        data = response.json()
+        items = data.get("item", [])
+        
+        for item in items:
+            all_books.append({
+                "title": item.get("title"),
+                "artist": item.get("author"),
+                "year": int(item.get("pubDate", "2000")[:4]),
+                
+                "score": item.get("customerReviewRank", 0) / 2 if item.get("customerReviewRank", 0) > 5 else item.get("customerReviewRank", 0),
+                "category": "book",
+                "genre": genre_name,
+                "salesPoint": item.get("salesPoint", 0) 
+            })
+        time.sleep(1) 
+        
+    return pd.DataFrame(all_books)
+
+
+df = get_balanced_bestseller()
+df.to_csv("book_data.csv", index=False, encoding='utf-8-sig')
+print("--- 데이터 수집 및 판매지수 포함 저장 완료 ---")
